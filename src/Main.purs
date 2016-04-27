c00ea32e6d12f359568858fe4a1b743809d01734
thimoteus <thimoteus@teknik.io>
2016-04-26 22:44:29 -0700

    working repl

--- src/Main.purs
+++ src/Main.purs
@@ -6,10 +6,19 @@ import SandScript.Parser (read)
 import SandScript.Eval (eval)
 import SandScript.Errors (ThrowsError, extractValue, trapError)
 
+import Control.Apply ((*>))
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, print)
+import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Exception (EXCEPTION)
 
-repl :: forall eff. String -> Eff ( console :: CONSOLE | eff ) Unit
+import Node.ReadLine as RL
+
+type Repl a = Eff ( console :: CONSOLE,  readline :: RL.READLINE, err :: EXCEPTION ) a
+
+replInterface :: Repl RL.Interface
+replInterface = RL.createConsoleInterface RL.noCompletion
+
+repl :: String -> Repl Unit
 repl s = do
   let evaled :: ThrowsError String
       evaled = show <$> (read s >>= eval)
@@ -17,7 +26,18 @@ repl s = do
       trapped = trapError evaled
       extracted :: String
       extracted = extractValue trapped
-  print extracted
+  log extracted
 
-main :: Eff ( console :: CONSOLE ) Unit
-main = repl "(mod 30 9)"
+main :: Repl Unit
+main = do
+   interface <- replInterface
+   RL.setPrompt ">> " 2 interface
+   RL.setLineHandler interface (handler interface)
+   RL.prompt interface
+
+handler :: RL.Interface -> String -> Repl Unit
+handler i =
+  case _ of
+       "exit" -> RL.close i
+       x -> repl x *> RL.prompt i
+
